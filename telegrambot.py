e93e5c16475a057a6a5f099f2a731fe6b362a250
Jere Lilja <jere.lilja@bitwise.fi>
2019-09-12 10:20:08 +0300

    Minor update to skiltua-handler & tab to space

--- telegrambot.py
+++ telegrambot.py
@@ -18,36 +18,39 @@ DOOR_OPEN_MSG   = 'Ovi on auki.'
 DOOR_CLOSED_MSG = 'Ovi on kiinni.'
 
 def setupDoorMonitor():
-	# Set GPIO mode to BCM (https://sourceforge.net/p/raspberry-gpio-python/wiki/BasicUsage/)
-	GPIO.setmode(GPIO.BCM)
+    # Set GPIO mode to BCM (https://sourceforge.net/p/raspberry-gpio-python/wiki/BasicUsage/)
+    GPIO.setmode(GPIO.BCM)
 
-	# Set pin as input and use pull-up (open = true, closed = false)
-	GPIO.setup(DOOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # Set pin as input and use pull-up (open = true, closed = false)
+    GPIO.setup(DOOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def getDoorStateMessage():
-	return DOOR_OPEN_MSG if GPIO.input(DOOR_PIN) else DOOR_CLOSED_MSG
+    return DOOR_OPEN_MSG if GPIO.input(DOOR_PIN) else DOOR_CLOSED_MSG
 
 def ovi(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text=getDoorStateMessage())
+    bot.sendMessage(chat_id=update.message.chat_id, text=getDoorStateMessage())
 
 def skiltaa(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text="skiltua")
+    msg = update.message
+    if msg.text is not None:
+        if 'skiltua' in msg.text.lower():
+            bot.sendMessage(chat_id=update.message.chat_id, text="skiltua")
 
 def main():
-	setupDoorMonitor()
+    setupDoorMonitor()
 
-	updater = Updater(cfg['TELEGRAM']['token'])
-	dispatcher = updater.dispatcher
-	logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    updater = Updater(cfg['TELEGRAM']['token'])
+    dispatcher = updater.dispatcher
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-	ovire = re.compile(r'^/ovi$', flags=re.IGNORECASE)
-	ovi_handler = RegexHandler(ovire, ovi)
-	dispatcher.add_handler(ovi_handler)
+    ovire = re.compile(r'^/ovi$', flags=re.IGNORECASE)
+    ovi_handler = RegexHandler(ovire, ovi)
+    dispatcher.add_handler(ovi_handler)
 
-	skiltu_handler = CommandHandler('skiltua', skiltaa)
-	dispatcher.add_handler(skiltu_handler)
+    skiltu_handler = MessageHandler(Filters.all, skiltaa)
+    dispatcher.add_handler(skiltu_handler)
 
-	updater.start_polling()
-	updater.idle()
+    updater.start_polling()
+    updater.idle()
 
 main()
